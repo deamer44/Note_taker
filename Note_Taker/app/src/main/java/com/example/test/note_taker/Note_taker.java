d13b663bc1c9d6fd37a6f37c62924cf198ed028e
Thomas Deamer <tom.deamer@googlemail.com>
2015-11-04 09:11:02 +0000

    trying to get sending data to python socket working locally.

--- Note_Taker/app/src/main/java/com/example/test/note_taker/Note_taker.java
+++ Note_Taker/app/src/main/java/com/example/test/note_taker/Note_taker.java
@@ -6,6 +6,12 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 public class Note_taker extends AppCompatActivity {
 
     @Override
@@ -21,6 +27,24 @@ public class Note_taker extends AppCompatActivity {
 
         EditText data = (EditText)findViewById(R.id.note_info);
         t.setText(data.getText());
+    }
+
+    public void  RunSocketClient(View view)
+    {
+        try
+        {
+        Socket client = new Socket("10.0.0.100",13377);
+            PrintWriter out = new PrintWriter(client.getOutputStream(),true);
+            EditText data = (EditText)findViewById(R.id.note_info);
+            TextView t = (TextView)findViewById(R.id.hel_id);
+            t.setText(data.getText());
+            out.println(data.getText());
+
+        }
+        catch (IOException e )
+        {
+
+        }
     }
 
 
